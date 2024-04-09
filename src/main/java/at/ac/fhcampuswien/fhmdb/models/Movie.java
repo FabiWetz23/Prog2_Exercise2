ee733ccb371dee878515ea6fbce1227b2ef9c29e
Fatemeh Javidi <fatemeh.javidi@stud.fh-campuswien.ac.at>
2024-04-09 23:13:46 +0200

    movie task

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -4,15 +4,44 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.UUID;
+
 public class Movie {
+    private final String id;
     private final String title;
     private final String description;
     private final List<Genre> genres;
+    private final int releaseYear;
+    private final String imgUrl;
+    private final int lengthInMinutes; // in minutes
+    private final List<String> directors = new ArrayList<>();
+    private final List<String> writers = new ArrayList<>();
+    private final List<String> mainCast = new ArrayList<>();
+    private final double rating; // 0-10
 
     public Movie(String title, String description, List<Genre> genres) {
+        this.id = UUID.randomUUID().toString();
         this.title = title;
         this.description = description;
         this.genres = genres;
+        this.releaseYear = 0;
+        this.imgUrl = "";
+        this.lengthInMinutes = 0;
+        this.rating = 0;
+    }
+    public Movie(String id, String title, String description, List<Genre> genres, int releaseYear, String imgUrl, int lengthInMinutes, double rating) {
+        if(id == null) {
+            this.id = UUID.randomUUID().toString();
+        } else {
+            this.id = id;
+        }
+        this.title = title;
+        this.description = description;
+        this.genres = genres;
+        this.releaseYear = releaseYear;
+        this.imgUrl = imgUrl;
+        this.lengthInMinutes = lengthInMinutes;
+        this.rating = rating;
     }
 
     @Override
@@ -29,6 +58,7 @@ public class Movie {
         return this.title.equals(other.title) && this.description.equals(other.description) && this.genres.equals(other.genres);
     }
 
+    public String getId() {return id; }
     public String getTitle() {
         return title;
     }
@@ -40,6 +70,35 @@ public class Movie {
     public List<Genre> getGenres() {
         return genres;
     }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public int getLengthInMinutes() {
+        return lengthInMinutes;
+    }
+
+    public List<String> getDirectors() {
+        return directors;
+    }
+
+    public List<String> getWriters() {
+        return writers;
+    }
+
+    public List<String> getMainCast() {
+        return mainCast;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
